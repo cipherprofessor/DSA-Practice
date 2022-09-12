887944baa37a9074afd7aaa2e8a6945abe51efdf
Cipher_Professor <mohsinmanzoor1913@gmail.com>
2022-09-12 20:11:49 +0530

    modified:   Random/sana.cpp 	modified:   Random/sana.exe

--- Random/sana.cpp
+++ Random/sana.cpp
@@ -118,20 +118,20 @@ int main()
 			{
 			 case 1:
 			 cout<<"\t\t--------Your Order---------\n";
-			 cout<<""<<quantity<<" "<<Cappuccino1;
+			 cout<<""<<quantity<<Cappuccino1;
 			 cout<<"\nYour Total Bill is"<<choice<<"\nYour Order Will be delivered in 40 Minutes";
 			 cout<<"\nThank you For Ordering From Coffee House \n";
 			 break;
 			 case 2:
 			 cout<<"\t\t--------Your Order---------\n";
-			 cout<<""<<quantity<<"  "<<Cappuccino2;
-			 cout<<"\nYour Total Bill is"<<choice<<"\nYour Order Will be delivered in 40 Minutes";
+			 cout<<""<<quantity<<Cappuccino2;
+			 cout<<"\nYour Total Bill is"<<choice<<"\nYour Order Will be ready in 40 Minutes";
 			 cout<<"\nThank you For Ordering From Coffee House \n";
 			 break;
 			 case 3:
 			 cout<<"\t\t--------Your Order---------\n";
-			 cout<<""<<quantity<<" "<<Cappuccino3;
-			 cout<<"\nYour Total Bill is"<<choice<<"\nYour Order Will be delivered in 40 Minutes";
+			 cout<<""<<quantity<<Cappuccino3;
+			 cout<<"\nYour Total Bill is"<<choice<<"\nYour Order Will be ready in 40 Minutes";
 			 cout<<"\nThank you For Ordering From Coffee House \n";
 			 break;
 
@@ -144,6 +144,78 @@ int main()
 			  goto beginning;
 			  //return 0;
 			}
-        }
+
  }
+}
+	else if(choice==3)
+	 {
+		cout<<"\n1  "<<Latte1<<"Rs 100"<<"\n";
+		cout<<"2  "<<Latte2<<"Rs 125"<<"\n";
+		cout<<"3  "<<Latte3<<"Rs150"<<"\n";
+		cout<<"\nPlease Enter which Latte you would like to have?:";
+		cin>>pchoice1;
+		if(pchoice1>=1 && pchoice1<=3)
+		{
+			cout<<"\nPlease Enter Quantity: ";
+			cin>>quantity;
+			switch(pchoice1)
+			{
+			case 1: choice = 100*quantity;
+			break;
+
+			case 2: choice = 125*quantity;
+			break;
+
+			case 3: choice = 150*quantity;
+			break;
+
+			}
+			system("CLS");
+			switch (pchoice1)
+			{
+			 case 1:
+			 cout<<"\t\t--------Your Order---------\n";
+			 cout<<""<<quantity<<Latte1;
+			 cout<<"\nYour Total Bill is"<<choice<<"\nYour Order Will be Ready in 40 Minutes";
+			 cout<<"\nThank you For Ordering From Coffee House \n";
+			 break;
+			 case 2:
+			 cout<<"\t\t--------Your Order---------\n";
+			 cout<<""<<quantity<<"  "<<Latte2;
+			 cout<<"\nYour Total Bill is"<<choice<<"\nYour Order Will be Ready in 40 Minutes";
+			 cout<<"\nThank you For Ordering From Coffee House \n";
+			 break;
+			 case 3:
+			 cout<<"\t\t--------Your Order---------\n";
+			 cout<<""<<quantity<<" "<<Latte3;
+			 cout<<"\nYour Total Bill is"<<choice<<"\nYour Order Will be Ready in 40 Minutes";
+			 cout<<"\nThank you For Ordering From Coffee House \n";
+			 break;
+
+
+			}
+			cout<<"Would you like to order anything else? Y / N:";
+			cin>>gotostart;
+			if(gotostart=='Y' || gotostart=='y')
+			{
+			  goto beginning;
+			  //return 0;
+			}
+			}
+			}
+			else
+			{
+				system("CLS");
+				cout<<"Please Select Right Option: \n";
+				cout<<"Would You like to Start the program again? Y / N: " ;
+				cin>>gotostart;
+
+				if(gotostart=='Y' || gotostart=='y')
+				{
+				  goto beginning;
+			  //return 0;
+			}
+			}
+
+    
 }
